c8c0de5697878870204fe95a7116511c511161d1
thebluescreen <marius.poncon@gmail.com>
2025-12-20 03:28:25 +0100

    tomorrow

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,5 @@
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 from cryptography.hazmat.primitives.asymmetric import ed25519
 import os, json, base64
 from cryptography.hazmat.primitives.ciphers import (
@@ -8,7 +9,7 @@ from cryptography.hazmat.primitives.ciphers import (
 def generate_ed25519_keypair(name):
 
     # --- PRIVATE KEY (raw bytes) ---
-    private_key = ed25519.Ed25519PrivateKey.generate()
+    private_key = Ed25519PrivateKey.generate()
 
     private_bytes = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
@@ -39,6 +40,7 @@ def generate_ed25519_keypair(name):
         json.dump({"algorithm": data["algorithm"], "name": data["name"], "public_key": data["keys"]["public"]}, f, indent=2)
 #generate_ed25519_keypair(name = input("Enter a name for the ed25519 key: "))
 
+
 def generate_aes_key(name):
     data = {
         "algorithm": "AES-256-GCM",
@@ -51,6 +53,8 @@ def generate_aes_key(name):
         json.dump(data, f, indent=2)
 #generate_aes_key(name = input("Enter a name for the AES key: "))
 
+# Retrive stored keys and ciphertext
+
 def retrieve_aes_key(name):
     with open(f"{name}_aes256gcm_key.json", "r", encoding="utf-8") as f:
         data = json.load(f)
@@ -59,14 +63,19 @@ def retrieve_aes_key(name):
 def retrieve_ed25519_private_key(name):
     with open(f"{name}_ed25519_private.json", "r", encoding="utf-8") as f:
         data = json.load(f)
-    return data["private_key"]
+    return serialization.load_pem_private_key(
+        data["private_key"].encode("utf-8"),
+        password=None,
+    )
 
 def retrieve_ed25519_public_key(name):
     with open(f"{name}_ed25519_public.json", "r", encoding="utf-8") as f:
         data = json.load(f)
-    return data["public_key"]
+    return serialization.load_pem_public_key(
+        data["public_key"].encode("utf-8")
+    )
 
-def retrieve_ciphertext(name):
+def retrieve_ciphertext(name, only_ciphertext=False):
     with open(f"{name}_encrypted_message.json", "r", encoding="utf-8") as f:
         data = json.load(f)
 
@@ -75,40 +84,46 @@ def retrieve_ciphertext(name):
     tag = base64.b64decode(data["tag"])
     associated_data = base64.b64decode(data["associated_data"])
 
+    if only_ciphertext:
+        return ciphertext
+
     return (iv, ciphertext, tag, associated_data)
 
-def encrypt(name:str, key:bytes, plaintext:str, associated_data:str):
-    # Generate a random 96-bit IV.
-    iv = os.urandom(12)
+# Encrypt and decrypt functions
 
-    # Construct an AES-GCM Cipher object with the given key and a
-    # randomly generated IV.
+def encrypt(name:str, key:bytes, plaintext:str, associated_data:str):
+    iv = os.urandom(12)
     encryptor = Cipher(
         algorithms.AES(key),
         modes.GCM(iv),
     ).encryptor()
 
+    keyname = input("Enter the name of the ed25519 key to sign the message: ")
+
     encryptor.authenticate_additional_data(associated_data)
 
-    # Encrypt the plaintext and get the associated ciphertext.
-    # GCM does not require padding.
     ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+
+    signature = retrieve_ed25519_private_key(keyname).sign(ciphertext)
+    print("Signature:", base64.b64encode(signature).decode("utf-8"))
+
     data = {
         "algorithm": "AES-256-GCM",
         "iv": base64.b64encode(iv).decode("utf-8"),
         "ciphertext": base64.b64encode(ciphertext).decode("utf-8"),
         "tag": base64.b64encode(encryptor.tag).decode("utf-8"),
-        "associated_data": base64.b64encode(associated_data).decode("utf-8")
+        "associated_data": base64.b64encode(associated_data).decode("utf-8"),
+        "signature": base64.b64encode(signature).decode("utf-8"),
+        "singing_public_key": base64.b64encode(retrieve_ed25519_public_key(keyname)).decode("utf-8")
     }
 
     with open(f"{name}_encrypted_message.json", "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2)
 
-    return (iv, ciphertext, encryptor.tag)
+    return {"iv":iv, "ciphertext":ciphertext, "encryptor":encryptor.tag}
 
 def decrypt(key:bytes):
-
-    iv, ciphertext, tag, associated_data = retrieve_ciphertext(name = input("Enter the name of the encrypted message to retrieve: "))
+    iv, ciphertext, tag, associated_data = retrieve_ciphertext(name = input("Enter the name of the encrypted message to retrieve: "), only_ciphertext=False)
 
     decryptor = Cipher(
         algorithms.AES(key),
